40e74f599e17baec4fc173fd66e97dd5f2489034
pallanghunor <pallang.hunor@students.jedlik.eu>
2025-01-06 11:03:17 +0100

    angular done

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,18 +2,27 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Film } from './models/film-model';
 import { FilmService } from './services/film.service';
-import { PopupComponent } from "./components/popup/popup.component";
+import { PopupComponent } from './components/popup/popup.component';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   imports: [RouterOutlet, PopupComponent, CommonModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   films: Film[] = [];
-  SelectedFilm: Film | undefined = undefined;
+  private _selectedFilm: Film | undefined = undefined;
+
+  get selectedFilm(): Film | undefined {
+    return this._selectedFilm;
+  }
+
+  set selectedFilm(film: Film | undefined) {
+    this._selectedFilm = film;
+    this.toggleBodyScroll();
+  }
 
   constructor(private filmService: FilmService) {}
 
@@ -22,17 +31,71 @@ export class AppComponent {
       next: (films) => {
         this.films = films;
       },
-      error: (error) => { console.error('Error fetching films:', error); }
+      error: (error) => {
+        console.error('Error fetching films:', error);
+      },
     });
   }
 
-  newFilm() {
+  private toggleBodyScroll() {
+    if (this._selectedFilm) {
+      document.body.style.overflow = 'hidden';
+    } else {
+      document.body.style.overflow = '';
+    }
+  }
 
+  newFilm() {
+    this.selectedFilm = {
+      id: undefined,
+      title: '',
+      director: '',
+      year: 0,
+      rating: 0,
+      genre: '',
+    };
   }
 
   editFilm(film: Film) {
-    this.SelectedFilm = JSON.parse(JSON.stringify(film));
+    this.selectedFilm = JSON.parse(JSON.stringify(film));
   }
 
+  deleteFilm(filmID: string) {
+    this.filmService.deleteFilm(filmID).subscribe({
+      next: () => {
+        this.films = this.films.filter((f) => f.id !== filmID);
+      },
+      error: (error) => {
+        console.error('Error deleting film:', error);
+      },
+    });
+  }
 
+  saveFilm(film: Film) {
+    if (film.id) {
+      this.filmService.updateFilm(film).subscribe({
+        next: (updatedFilm) => {
+          const index = this.films.findIndex((f) => f.id === updatedFilm.id);
+          this.films[index] = updatedFilm;
+        },
+        error: (error) => {
+          console.error('Error updating film:', error);
+        },
+      });
+    } else {
+      this.filmService.addFilm(film).subscribe({
+        next: (newFilm) => {
+          this.films.push(newFilm);
+        },
+        error: (error) => {
+          console.error('Error creating film:', error);
+        },
+      });
+    }
+    this.selectedFilm = undefined;
+  }
+
+  onCancel() {
+    this.selectedFilm = undefined;
+  }
 }
--- src/app/components/popup/popup.component.ts
+++ src/app/components/popup/popup.component.ts
@@ -1,25 +1,26 @@
 import { Component, EventEmitter, Input, Output } from '@angular/core';
 import { Film } from '../../models/film-model';
-import { NgForm } from '@angular/forms';
+import { FormsModule, NgForm } from '@angular/forms';
 import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-popup',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './popup.component.html',
   styleUrl: './popup.component.css'
 })
 export class PopupComponent {
   @Input() film: Film | undefined = undefined;
   @Output() save = new EventEmitter<Film>();
+  @Output() cancel = new EventEmitter<void>();
 
-  onSubmit(form: NgForm) {
+  onSave(form: NgForm) {
     if (form.valid) {
       this.save.emit(this.film);
     }
   }
 
   onCancel() {
-    this.film = undefined;
+    this.cancel.emit();
   }
 }
